56d047df5d133d8c5ae86157b051d348d4683a44
sani <saniku@gmail.com>
2022-10-09 13:27:58 +0530

    till by 09_10_22

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\CloudHostingProduct;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('command', function () {
-	
-	/* php artisan migrate */
-    \Artisan::call('make:controller EmailController');
+    Artisan::call('make:controller EmailController');
     dd("Done");
+});
+
+Route::get('send-email', function(){
+    $mailData = [
+        "name" => "Test NAME",
+        "dob" => "12/12/1990"
+    ];
+
+    Mail::to("saniku251@gmail.com")->send(new CloudHostingProduct($mailData));
+
+    dd("Mail Sent Successfully!");
 });
